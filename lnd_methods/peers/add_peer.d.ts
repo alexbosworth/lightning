b34c77b753d9465d69d6d6ecfdbd5ada14ad411c
Bence Knáb <benceknab@pm.me>
2021-01-05 17:14:35 +0100

    Add add_peer types

---
+++ lnd_methods/peers/add_peer.d.ts
@@ -0,0 +1,25 @@
+import {AuthenticatedLndMethod} from '../../typescript/shared';
+
+export type AddPeerArgs = {
+  /** Add Peer as Temporary Peer, default: `false` */
+  is_temporary?: boolean;
+  /** Public Key Hex */
+  public_key: string;
+  /** Retry Count */
+  retry_count?: number;
+  /** Delay Retry By Milliseconds */
+  retry_delay?: number;
+  /** Host Network Address And Optional Port, format: ip:port */
+  socket: string;
+  /** Connection Attempt Timeout Milliseconds, not supported in LND 0.11.1 and below */
+  timeout?: number;
+};
+
+/**
+ * Add a peer if possible (not self, or already connected)
+ *
+ * Requires `peers:write` permission
+ *
+ * `timeout` is not supported in LND 0.11.1 and below
+ */
+export const addPeer: AuthenticatedLndMethod<AddPeerArgs>;
