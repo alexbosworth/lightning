0da78dc080cac7046fa6e1a08c1169fb71de3c0d
Bence Knáb <benceknab@pm.me>
2021-01-06 15:05:03 +0100

    Move type definitions next to their source

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -1,6 +1,1 @@
-export * from './authenticated_lnd_grpc';
-export * from './emit_grpc_events';
-export * from './grpc_router';
-export * from './lnd_gateway';
 export * from './shared';
-export * from './unauthenticated_lnd_grpc';
