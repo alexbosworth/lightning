05a8474679a8a50176f58f2b6363582cc17e732c
Bence Knáb <benceknab@pm.me>
2021-01-18 13:27:27 +0100

    Add types for getInvoices method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {createHodlInvoice} = require('./lnd_methods');
 const {createInvoice} = require('./lnd_methods');
 const {emitGrpcEvents} = require('./lnd_gateway');
 const {getInvoice} = require('./lnd_methods');
+const {getInvoices} = require('./lnd_methods');
 const {grpcRouter} = require('./lnd_gateway');
 const {lndGateway} = require('./lnd_gateway');
 const {unauthenticatedLndGrpc} = require('./lnd_grpc');
@@ -15,6 +16,7 @@ module.exports = {
   createInvoice,
   emitGrpcEvents,
   getInvoice,
+  getInvoices,
   grpcRouter,
   lndGateway,
   unauthenticatedLndGrpc,
--- lnd_methods/invoices/index.d.ts
+++ lnd_methods/invoices/index.d.ts
@@ -2,3 +2,4 @@ export * from './cancel_hodl_invoice';
 export * from './create_hodl_invoice';
 export * from './create_invoice';
 export * from './get_invoice';
+export * from './get_invoices';
