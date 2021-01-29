0291890d54d5cc769169020cbf0683b4003b262d
Ben Knáb <benknab@pm.me>
2021-01-29 15:16:47 +0100

    Fix getBackup test

--- test/typescript/get_backup.test-d.ts
+++ test/typescript/get_backup.test-d.ts
@@ -9,7 +9,7 @@ const transaction_vout = 0;
 expectError(getBackup());
 expectError(getBackup({}));
 expectError(getBackup({transaction_id}));
-expectError(getBackup({transaction_id, transaction_vouts}));
+expectError(getBackup({transaction_id, transaction_vout}));
 expectError(getBackup({lnd}));
 expectError(getBackup({lnd, transaction_id}));
 expectError(getBackup({lnd, transaction_vout}));
