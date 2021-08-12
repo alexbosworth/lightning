4fceb2deeed0eab585a7263e860bd1b906ead0a1
Alex Bosworth <alex.bosworth@gmail.com>
2021-08-12 09:38:13 -0700

    match wallet state parsing to standard naming

---
+++ lnd_responses/rpc_wallet_state_as_state.js
@@ -0,0 +1,50 @@
+const stateAbsent = 'NON_EXISTING';
+const stateActive = 'RPC_ACTIVE';
+const stateLocked = 'LOCKED';
+const stateStarting = 'UNLOCKED';
+const stateWaiting = 'WAITING_TO_START';
+
+/** Interpret state as wallet status
+
+  {
+    state: <WalletState String>
+  }
+
+  @returns
+  {
+    [is_absent]: <Wallet Not Created Bool>
+    [is_active]: <Wallet Is Active Bool>
+    [is_locked]: <Wallet File Encrypted And Wallet Not Active Bool>
+    [is_starting]: <Wallet Is Starting Up Bool>
+    [is_waiting]: <Wallet Is Waiting To Start Bool>
+  }
+*/
+module.exports = args => {
+  if (!args) {
+    throw new Error('ExpectedStateResponse');
+  }
+
+  if (!args.state) {
+    throw new Error('ExpectedStateInStateResponse');
+  }
+
+  switch (args.state) {
+  case stateAbsent:
+    return {is_absent: true};
+
+  case stateActive:
+    return {is_active: true};
+
+  case stateLocked:
+    return {is_locked: true};
+
+  case stateStarting:
+    return {is_starting: true};
+
+  case stateWaiting:
+    return {is_waiting: true};
+
+  default:
+    return {};
+  }
+};
---
+++ test/lnd_responses/test_rpc_wallet_state_as_state.js
@@ -0,0 +1,65 @@
+const {test} = require('@alexbosworth/tap');
+
+const {rpcWalletStateAsState} = require('./../../lnd_responses');
+
+const makeExpected = overrides => {
+  const expected = {};
+
+  Object.keys(overrides).forEach(k => expected[k] = overrides[k]);
+
+  return expected;
+};
+
+const tests = [
+  {
+    description: 'No response throws an error',
+    error: 'ExpectedStateResponse',
+  },
+  {
+    args: {},
+    description: 'No state in response throws an error',
+    error: 'ExpectedStateInStateResponse',
+  },
+  {
+    args: {state: 'NON_EXISTING'},
+    description: 'Wallet does not exist',
+    expected: makeExpected({is_absent: true}),
+  },
+  {
+    args: {state: 'RPC_ACTIVE'},
+    description: 'Wallet is ready',
+    expected: makeExpected({is_active: true}),
+  },
+  {
+    args: {state: 'LOCKED'},
+    description: 'Wallet file is encrypted',
+    expected: makeExpected({is_locked: true}),
+  },
+  {
+    args: {state: 'UNLOCKED'},
+    description: 'Wallet file was decrypted and wallet is starting up',
+    expected: makeExpected({is_starting: true}),
+  },
+  {
+    args: {state: 'WAITING_TO_START'},
+    description: 'Wallet is waiting for leader election to start',
+    expected: makeExpected({is_waiting: true}),
+  },
+  {
+    args: {state: 'UNCOVERED'},
+    description: 'State received is uncovered by mapping',
+    expected: makeExpected({}),
+  },
+];
+
+tests.forEach(({args, description, error, expected}) => {
+  return test(({end, equal, strictSame, throws}) => {
+    if (!!error) {
+      throws(() => rpcWalletStateAsState(args), new Error(error), 'Got error');
+    } else {
+      strictSame(rpcWalletStateAsState(args), expected, 'Got expected');
+    }
+
+    return end();
+  })
+});
