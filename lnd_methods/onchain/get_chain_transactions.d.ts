bbc3144a050961bf1e361d16af5c8b504edffbc9
Ben Knáb <benknab@pm.me>
2022-08-27 10:14:46 +0200

    Add inputs to chain transactions

--- lnd_methods/onchain/get_chain_transactions.d.ts
+++ lnd_methods/onchain/get_chain_transactions.d.ts
@@ -1,6 +1,7 @@
 import {
   AuthenticatedLightningArgs,
   AuthenticatedLightningMethod,
+  TransactionInput,
 } from '../../typescript';
 
 export type GetChainTransactionsArgs = AuthenticatedLightningArgs<{
@@ -10,39 +11,51 @@ export type GetChainTransactionsArgs = AuthenticatedLightningArgs<{
   before?: number;
 }>;
 
-export type GetChainTransactionsResult = {
-  transactions: {
-    /** Block Hash */
-    block_id?: string;
-    /** Confirmation Count */
-    confirmation_count?: number;
-    /** Confirmation Block Height */
-    confirmation_height?: number;
-    /** Created ISO 8601 Date */
-    created_at: string;
-    /** Transaction Label */
-    description?: string;
-    /** Fees Paid Tokens */
-    fee?: number;
-    /** Transaction Id */
-    id: string;
-    /** Is Confirmed */
-    is_confirmed: boolean;
-    /** Transaction Outbound */
-    is_outgoing: boolean;
-    /** Addresses */
-    output_addresses: string[];
-    /** Tokens Including Fee */
-    tokens: number;
-    /** Raw Transaction Hex */
-    transaction?: string;
+export type ChainTransaction = {
+  /** Block Hash */
+  block_id?: string;
+  /** Confirmation Count */
+  confirmation_count?: number;
+  /** Confirmation Block Height */
+  confirmation_height?: number;
+  /** Created ISO 8601 Date */
+  created_at: string;
+  /** Transaction Label */
+  description?: string;
+  /** Fees Paid Tokens */
+  fee?: number;
+  /** Transaction Id */
+  id: string;
+  inputs: {
+    /** Spent Outpoint is Local */
+    is_local: boolean;
+    /** Transaction Id Hex */
+    transaction_id: string;
+    /** Transaction Output Index */
+    transaction_vout: number;
   }[];
+  /** Is Confirmed */
+  is_confirmed: boolean;
+  /** Transaction Outbound */
+  is_outgoing: boolean;
+  /** Addresses */
+  output_addresses: string[];
+  /** Tokens Including Fee */
+  tokens: number;
+  /** Raw Transaction Hex */
+  transaction?: string;
+};
+
+export type GetChainTransactionsResult = {
+  transactions: ChainTransaction[];
 };
 
 /**
  * Get chain transactions.
  *
  * Requires `onchain:read` permission
+ *
+ * `inputs` are not supported on LND 0.15.0 and below
  */
 export const getChainTransactions: AuthenticatedLightningMethod<
   GetChainTransactionsArgs,
--- lnd_methods/onchain/subscribe_to_transactions.d.ts
+++ lnd_methods/onchain/subscribe_to_transactions.d.ts
@@ -1,33 +1,19 @@
-import {AuthenticatedLightningSubscription} from '../../typescript';
+import {
+  AuthenticatedLightningSubscription,
+  TransactionInput,
+} from '../../typescript';
+import {ChainTransaction} from './get_chain_transactions';
 
-export type SubscribeToTransactionsChainTransactionEvent = {
-  /** Block Hash */
-  block_id?: string;
-  /** Confirmation Count */
-  confirmation_count?: number;
-  /** Confirmation Block Height */
-  confirmation_height?: number;
-  /** Created ISO 8601 Date */
-  created_at: string;
-  /** Fees Paid Tokens */
-  fee?: number;
-  /** Transaction Id */
-  id: string;
-  /** Is Confirmed */
-  is_confirmed: boolean;
-  /** Transaction Outbound */
-  is_outgoing: boolean;
-  /** Addresses */
-  output_addresses: string[];
-  /** Tokens Including Fee */
-  tokens: number;
-  /** Raw Transaction Hex */
-  transaction?: string;
-};
+export type SubscribeToTransactionsChainTransactionEvent = Omit<
+  ChainTransaction,
+  'description'
+>;
 
 /**
  * Subscribe to transactions
  *
  * Requires `onchain:read` permission
+ *
+ * `inputs` are not supported on LND 0.15.0 and below
  */
 export const subscribeToTransactions: AuthenticatedLightningSubscription;
