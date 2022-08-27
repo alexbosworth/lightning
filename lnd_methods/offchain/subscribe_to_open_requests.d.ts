66061cbedecc908b942fadf3d48a56a74fad9a59
Ben Knáb <benknab@pm.me>
2022-08-27 10:00:28 +0200

    Add is_trusted_funding to subscribe_to_open_requests
    Fix missing `is_private` field from result

--- lnd_methods/offchain/subscribe_to_open_requests.d.ts
+++ lnd_methods/offchain/subscribe_to_open_requests.d.ts
@@ -5,6 +5,8 @@ export type SubscribeToOpenRequestsChannelRequestEvent = {
   accept: (args: {
     /** Restrict Coop Close To Address */
     cooperative_close_address?: string;
+    /** Accept Funding as Trusted */
+    is_trusted_funding?: boolean;
     /** Required Confirmations Before Channel Open */
     min_confirmations?: number;
     /** Peer Unilateral Balance Output CSV Delay */
@@ -28,6 +30,10 @@ export type SubscribeToOpenRequestsChannelRequestEvent = {
   csv_delay: number;
   /** Request Id Hex */
   id: string;
+  /** Incoming Channel Is Private Bool */
+  is_private: boolean;
+  /** Request Immediate Trusted Funding */
+  is_trusted_funding: boolean;
   /** Channel Local Tokens Balance */
   local_balance: number;
   /** Channel Local Reserve Tokens */
@@ -61,5 +67,7 @@ channel requests after a short delay.
 listeners to `channel_request`
  *
  * LND 0.11.1 and below do not support `accept` or `reject` arguments
+ * 
+ * LND 0.15.0 and below do not support `is_trusted_funding`
  */
 export const subscribeToOpenRequests: AuthenticatedLightningSubscription;
