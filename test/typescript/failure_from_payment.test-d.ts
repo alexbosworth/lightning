080f20f3877fd1fef7c6897836fdc6353e111950
Juan P Lopez <dolcalmi@gmail.com>
2023-05-01 01:35:43 -0500

    fix: typescript tests

--- test/typescript/failure_from_payment.test-d.ts
+++ test/typescript/failure_from_payment.test-d.ts
@@ -1,41 +1,49 @@
 import {expectError, expectType} from 'tsd';
 import {failureFromPayment} from '../../lnd_responses/failure_from_payment';
 
+const payment_hash = Buffer.alloc(32).toString('hex')
+
 expectError(failureFromPayment());
 expectError(failureFromPayment({}));
-expectError(failureFromPayment({failure_reason: 'invalid failure reason'}));
+expectError(failureFromPayment({payment_hash, failure_reason: 'invalid failure reason'}));
 
 expectType<{
+  id: string;
   is_insufficient_balance: boolean;
   is_invalid_payment: boolean;
   is_pathfinding_timeout: boolean;
   is_route_not_found: boolean;
-}>(failureFromPayment({failure_reason: 'FAILURE_REASON_NONE'}));
+}>(failureFromPayment({payment_hash, failure_reason: 'FAILURE_REASON_NONE'}));
 expectType<{
+  id: string;
   is_insufficient_balance: true;
   is_invalid_payment: false;
   is_pathfinding_timeout: false;
   is_route_not_found: false;
-}>(failureFromPayment({failure_reason: 'FAILURE_REASON_INSUFFICIENT_BALANCE'}));
+}>(failureFromPayment({payment_hash, failure_reason: 'FAILURE_REASON_INSUFFICIENT_BALANCE'}));
 expectType<{
+  id: string;
   is_insufficient_balance: false;
   is_invalid_payment: true;
   is_pathfinding_timeout: false;
   is_route_not_found: false;
 }>(
   failureFromPayment({
+    payment_hash,
     failure_reason: 'FAILURE_REASON_INCORRECT_PAYMENT_DETAILS',
   })
 );
 expectType<{
+  id: string;
   is_insufficient_balance: false;
   is_invalid_payment: false;
   is_pathfinding_timeout: true;
   is_route_not_found: false;
-}>(failureFromPayment({failure_reason: 'FAILURE_REASON_TIMEOUT'}));
+}>(failureFromPayment({payment_hash, failure_reason: 'FAILURE_REASON_TIMEOUT'}));
 expectType<{
+  id: string;
   is_insufficient_balance: false;
   is_invalid_payment: false;
   is_pathfinding_timeout: false;
   is_route_not_found: true;
-}>(failureFromPayment({failure_reason: 'FAILURE_REASON_NO_ROUTE'}));
+}>(failureFromPayment({payment_hash, failure_reason: 'FAILURE_REASON_NO_ROUTE'}));
