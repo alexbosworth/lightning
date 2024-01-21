0c9ae42a9c78ce3079b78c380363376cd7ac2171
Ben Knáb <benknab@pm.me>
2024-01-21 11:58:28 +0100

    Add getConfiguration types

---
+++ lnd_methods/info/get_configuration.d.ts
@@ -0,0 +1,30 @@
+import type {
+  AuthenticatedLightningArgs,
+  AuthenticatedLightningMethod,
+} from '../../typescript';
+
+export type GetConfigurationArgs = AuthenticatedLightningArgs;
+
+export type GetConfigurationResult = {
+  /** Log Line String */
+  log: string[];
+  options: {
+    /** Option Type String */
+    type: string;
+    /** Option Value String */
+    value: string;
+  }[];
+};
+
+/**
+ * Get the current configuration file settings and the output log
+ *
+ * Requires `info:read`, `offchain:read`, `onchain:read`, `peers:read`
+  permissions
+ *
+ * This method is not supported on LND 0.17.3 and below
+ */
+export const getConfiguration: AuthenticatedLightningMethod<
+  GetConfigurationArgs,
+  GetConfigurationResult
+>;
