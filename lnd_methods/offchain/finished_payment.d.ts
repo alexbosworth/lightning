7b29ca630ad4bf555ca06fc7bf0a59ffb2d670bf
Nitesh Balusu <niteshbalusu@icloud.com>
2024-09-04 14:05:58 -0400

    fix typescript types

--- lnd_methods/offchain/finished_payment.d.ts
+++ lnd_methods/offchain/finished_payment.d.ts
@@ -27,6 +27,8 @@ export type FinishedPaymentArgs = AuthenticatedLightningArgs<{
     }[];
     /** Payment Hash Hex String */
     id: string;
+    /** Payment Index String */
+    index: string;
     /** Total Millitokens Paid String */
     mtokens: string;
     paths: {
